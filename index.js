751e54d5cfcdf3500c8079bef080e1da1518e54d
Olivier Audard <olivier@audard.net>
2022-12-06 23:49:32 +0100

    Implement unit-tests

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 'use strict';
 
-const { htmlTag } = require('hexo-util');
+const dailymotionTag = require('./lib/dailymotion.js')
 
-
-/**
-* Dailymotion tag
-*
-* Syntax:
-*   {% dailymotion [player:player_id] [video:video_id] [playlist:playlist_id]  %}
-*/
-hexo.extend.tag.register('dailymotion', function (args) {
-
-  const arg = args.join(' ');
-
-  const [, player=null] = arg.match(/\s*player:(\w+)/i) || [];
-  const [, video=null] = arg.match(/\s*video:(\w+)/i) || [];
-  const [, playlist=null] = arg.match(/\s*playlist:(\w+)/i) || [];
-
-  const playerPath = player ? `player/${player}.js` : `player.js`;
-  
-  return htmlTag('script', {
-    src: `https://geo.dailymotion.com/${playerPath}`, 
-    class: 'video-container',
-    ...(video ? {'data-video': video} : null),
-    ...(playlist ? {'data-playlist': playlist} : null),
-  }, '', false);
-});
+hexo.extend.tag.register('dailymotion', dailymotionTag);
